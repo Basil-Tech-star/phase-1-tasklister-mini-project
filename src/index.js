1322768ff0978b61a366c7348ce6a95c40d39fb6
Basil Wabuke <babazwabuke4@gmail.com>
2024-10-07 15:01:57 +0300

    added index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e)=>
+  { e.preventDefault();
+    createTodo(e.target["new-task-description"].value, e.target["priority"].value);
+    form.reset();
+  })
 });
+function createTodo(todo, priority)
+{
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "Delete";
+  li.textContent = todo;
+  li.appendChild(btn);
+
+  if (priority === "high"){
+    li.style.color = "red";
+  }else if(priority === "medium"){
+    li.style.color = "yellow";
+  }else if(priority === "low"){
+    li.style.color = "green";
+  }
+  document.querySelector("#tasks").appendChild(li);
+}
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
